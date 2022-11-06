f16e7b33642e8fe4e06cac326488a45b37ea304c
mirrer <alsejr1004@gmail.com>
2022-11-06 21:06:26 +0900

    Fix: lambda

--- lambda/index.js
+++ lambda/index.js
@@ -4,7 +4,7 @@ const sharp = require('sharp');
 const s3 = new AWS.S3();
 
 exports.handler = async (event, context, callback) => {
-  const Bucket = event.Records[0].s3.bucket.name; // 버킷 이름  
+  const Bucket = event.Records[0].s3.bucket.name; // react-nodebird-s3
   const Key = decodeURIComponent(event.Records[0].s3.object.key); // original/12312312_abc.png
   console.log(Bucket, Key);
   const filename = Key.split('/')[Key.split('/').length - 1];
@@ -23,12 +23,11 @@ exports.handler = async (event, context, callback) => {
       Bucket,
       Key: `thumb/${filename}`,
       Body: resizedImage,
-    }).promise(); 
+    }).promise();
     console.log('put', resizedImage.length);
     return callback(null, `thumb/${filename}`);
   } catch (error) {
-    console.log(error);
+    console.error(error)
     return callback(error);
   }
 }
-
