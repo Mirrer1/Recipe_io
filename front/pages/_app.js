b88f3ab1294cc4d1a0a30f1e6e4a5f53e695b97e
mirrer <alsejr1004@gmail.com>
2022-11-12 19:24:34 +0900

    Fix: App.js Meta tag

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -12,11 +12,6 @@ const App = ({ Component }) => {
       <Head>
         <meta charSet="utf-8" />
         <title>Recipe.io</title>
-        <meta name='description' content={'간편하고 다양한 요리 레시피에 접근할 수 있는 레시피 공유 SNS'}/>        
-        <meta property='og:title' content={'Recipe.io'} />
-        <meta property='og:description' content={'간편하고 다양한 요리 레시피에 접근할 수 있는 레시피 공유 SNS'} />                
-        <meta property="og:image" content={'https://recipeio.ga/favicon.ico'} />
-        <meta property='og:url' content={'https://recipeio.ga/'} />
       </Head>
       <Component />
     </>
